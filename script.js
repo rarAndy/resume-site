a882a092e17d7ab74cf815a350d84f065313144d
Andy <andrewryan128@gmail.com>
2023-06-05 13:43:12 -0400

    added project details

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function hello() {
-    console.log('ok')
-}
-
-hello()
+var wrapper = document.querySelector('.wrapper');
+var projectContainers = document.querySelectorAll('.project');
+projectContainers.forEach(function (project) {
+    project.addEventListener('click', function () {
+        console.log('clicked');
+        wrapper.classList.add('active');
+        project.querySelector('.project-description').classList.add('active');
+    });
+});
+wrapper.addEventListener('click', function () {
+    wrapper.classList.remove('active');
+    document.querySelectorAll('.project-description').forEach(function (e) {
+        e.classList.remove('active');
+    });
+});
--- script.ts
+++ script.ts
@@ -1,6 +1,18 @@
-function printName(obj: { first: string; last?: string }) {
-  // ...
-}
-// Both OK
-printName({ first: "Bob" });
-printName({ first: "Alice", last: "Alisson" });
+const wrapper = document.querySelector('.wrapper')
+const projectContainers = document.querySelectorAll('.project')
+
+projectContainers.forEach((project) => {
+    project.addEventListener('click', () => {
+        console.log('clicked')
+        wrapper.classList.add('active')
+        project.querySelector('.project-description').classList.add('active')
+    }) 
+})
+
+wrapper.addEventListener('click', () => {
+    wrapper.classList.remove('active')
+    document.querySelectorAll('.project-description').forEach((e) => {
+        e.classList.remove('active')
+    })
+})
+
